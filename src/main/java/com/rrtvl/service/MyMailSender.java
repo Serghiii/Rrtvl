0655f1bc6087505d6bf9de4be8a842b6bf4ae60a
Serhii <sv.sergius@gmail.com>
2020-08-10 16:56:00 +0300

    rrtvl

--- src/main/java/com/rrtvl/service/MyMailSender.java
+++ src/main/java/com/rrtvl/service/MyMailSender.java
@@ -7,7 +7,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.internet.MimeMessage;
-1
+
 @Service
 public class MyMailSender {
 
